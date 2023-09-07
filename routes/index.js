c9a8de537618cd8243b2cdd7f8d7062f85932cad
OnyeEbube <okontaonyeka@gmail.com>
2023-09-07 06:04:06 -0700

    Adding solutions

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,15 @@ const AppController = require('../controllers/AppController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/user/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
